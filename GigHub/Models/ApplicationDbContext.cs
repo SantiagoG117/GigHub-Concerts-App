985e6b59acb9d49e0fe962c7627918f23665ebc6
Santiago Garcia <garc0254@algonquinlive.com>
2024-02-26 18:20:59 -0500

    Create Gig model

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        //DbSets
+        public DbSet<Gig> Gigs { get; set; } //Access to the Gig table
+        public DbSet<Genre> Genres { get; set; } //Access to the Genres class
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
