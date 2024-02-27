f93ad16c2952ee9d326487901a4bb2363b529dac
Santiago Garcia <garc0254@algonquinlive.com>
2024-02-27 18:25:53 -0500

    Create form for adding new gigs.

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.View_Models;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        //Create access to the Database
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            //Create the model
+            var model = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            //Send the model to the view
+            return View(model);
+        }
+
+        public ActionResult SaveGig(GigFormViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ GigHub/View Models/GigFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+
+namespace GigHub.View_Models
+{
+    /// <summary>
+    /// Model designed for presentation purposes of the Gig form.
+    /// </summary>
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+        public int GenreId { get; set; } 
+        public IList<Genre> Genres { get; set; }
+
+
+    }
+}
