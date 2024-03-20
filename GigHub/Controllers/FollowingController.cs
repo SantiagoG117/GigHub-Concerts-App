ef896df56d82e9747203ad18e68eeb5066e4158d
Santiago Garcia <garc0254@algonquinlive.com>
2024-03-19 20:58:31 -0400

    Created "Artist I am Following" feature

---
+++ GigHub/Controllers/FollowingController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.View_Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class FollowingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Following
+        [Authorize]
+        public ActionResult Following()
+        {
+            //Get the id of the current user
+            var curUserId = User.Identity.GetUserId();
+
+            //Get the artist that the current user is following
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == curUserId)
+                .Select(f => f.Artist)
+                .ToList();
+
+            //Build the model:
+            var model = new FollowingViewModel
+            {
+                Artists = artists,
+                IsAuthenticatedUser = User.Identity.IsAuthenticated
+            };
+
+            //Send the model the view:
+            return View(model);
+        }
+    }
+}
---
+++ GigHub/View Models/FollowingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GigHub.Models;
+
+namespace GigHub.View_Models
+{
+    public class FollowingViewModel
+    {
+        public IList<ApplicationUser> Artists { get; set; }
+        public bool IsAuthenticatedUser { get; set; }
+    }
+}
