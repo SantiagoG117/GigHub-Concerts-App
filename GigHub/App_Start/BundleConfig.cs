7fe019e7ccce23428b22ecb29839966769aa662a
Santiago Garcia <garc0254@algonquinlive.com>
2024-04-11 19:26:58 -0400

    Improve how datetime values are displayed in the notifications with moment.js

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -3,35 +3,35 @@ using System.Web.Optimization;
 
 namespace GigHub
 {
-    public class  BundleConfig
+    public class BundleConfig
     {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-min.js",
-                        "~/Scripts/moment.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/underscore-min.js",
+                "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/popper.min.js",
-                        "~/Scripts/bootstrap.min.js",
-                        "~/Scripts/bootbox.min.js"
-                        ));
+                "~/Scripts/popper.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootbox.min.js"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/animate.css",
-                      "~/Content/bounceInDown.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css",
+                "~/Content/animate.css",
+                "~/Content/bounceInDown.css"));
         }
     }
 }
